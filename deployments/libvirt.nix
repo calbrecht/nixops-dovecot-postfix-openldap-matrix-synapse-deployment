7647e5570054d55e28b9de1c270704cd39ca57a9
Christian Albrecht <christian.albrecht@mayflower.de>
2019-03-04 11:34:23 +0100

    Add power to the libvirt

--- deployments/libvirt.nix
+++ deployments/libvirt.nix
@@ -1,7 +1,14 @@
 {
   ngse = { config, pkgs, ... }: rec
   {
-    deployment.targetEnv = "libvirtd";
+    deployment = {
+      targetEnv = "libvirtd";
+      libvirtd = {
+        headless = true;
+        memorySize = 2048;
+        vcpu = 2;
+      };
+    };
     networking.hostName = "ngse.dedyn.io";
   };
 }
