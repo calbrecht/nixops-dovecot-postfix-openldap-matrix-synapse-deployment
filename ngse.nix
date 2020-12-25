8429e6e9dd169d8cf6a9b5892d604b1a367c8725
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-25 10:04:27 +0100

    formatting ngse.nix

--- ngse.nix
+++ ngse.nix
@@ -2,88 +2,88 @@
   network.description = "Virtual server";
 
   ngse = { config, pkgs, ... }:
-  {
-    imports = [
-      ./users.nix
-      ./acme.nix
-      ./openldap.nix
-      ./postfix.nix
-      ./dovecot.nix
-      ./matterbridge.nix
-      ./matrix-synapse.nix
-    ];
+    {
+      imports = [
+        ./users.nix
+        ./acme.nix
+        ./openldap.nix
+        ./postfix.nix
+        ./dovecot.nix
+        ./matterbridge.nix
+        ./matrix-synapse.nix
+      ];
 
-    environment.systemPackages = with pkgs; [
-      kitty
-      sqlite
-    ];
+      environment.systemPackages = with pkgs; [
+        kitty
+        sqlite
+      ];
 
-    nixpkgs = {
-      config = {
-        allowBroken = true;
-        allowUnfree = true;
+      nixpkgs = {
+        config = {
+          allowBroken = true;
+          allowUnfree = true;
+        };
+        system = "x86_64-linux";
       };
-      system = "x86_64-linux";
-    };
 
-    programs = {
-      zsh = {
-        enable = true;
-        interactiveShellInit = ''
-          precmd_functions=( vcs_info )
+      programs = {
+        zsh = {
+          enable = true;
+          interactiveShellInit = ''
+            precmd_functions=( vcs_info )
+          '';
+          promptInit = ''
+                    setopt prompt_subst
+
+                    zstyle ':vcs_info:*' enable git
+                zstyle ':vcs_info:git*:*' get-revision true
+                zstyle ':vcs_info:git*:*' check-for-changes true
+
+                # hash changes branch misc
+                zstyle ':vcs_info:git*' formats "(%s) %12.12i %c%u %b%m"
+                zstyle ':vcs_info:git*' actionformats "(%s|%a) %12.12i %c%u %b%m"
+
+                autoload -Uz vcs_info
+
+                    export PROMPT='%f%F{%(!.red.green)}%m%f %F{yellow}%~%f $vcs_info_msg_0_%E
+            %F{%(!.red.green)}%#%f%E '
+          '';
+        };
+      };
+
+      security.sudo.wheelNeedsPassword = true;
+
+      services = {
+        ntp.enable = false;
+        chrony.enable = false;
+
+        openssh = {
+          enable = true;
+          allowSFTP = false;
+          permitRootLogin = "without-password";
+          passwordAuthentication = false;
+          challengeResponseAuthentication = false;
+        };
+
+        xserver.enable = false;
+      };
+
+      networking = {
+        extraHosts = ''
+          127.0.0.2 ${config.networking.hostName}.${config.networking.domain}
+          127.0.0.3 matrix.${config.networking.hostName}.${config.networking.domain}
         '';
-        promptInit = ''
-          setopt prompt_subst
-  
-          zstyle ':vcs_info:*' enable git
-  	  zstyle ':vcs_info:git*:*' get-revision true
-  	  zstyle ':vcs_info:git*:*' check-for-changes true
-  
-  	  # hash changes branch misc
-  	  zstyle ':vcs_info:git*' formats "(%s) %12.12i %c%u %b%m"
-  	  zstyle ':vcs_info:git*' actionformats "(%s|%a) %12.12i %c%u %b%m"
-  
-  	  autoload -Uz vcs_info
-  	
-          export PROMPT='%f%F{%(!.red.green)}%m%f %F{yellow}%~%f $vcs_info_msg_0_%E
-  %F{%(!.red.green)}%#%f%E '
-        '';
-      };
-    };
-
-    security.sudo.wheelNeedsPassword = true;
-
-    services = {
-      ntp.enable = false;
-      chrony.enable = false;
-  
-      openssh = {
-        enable = true;
-        allowSFTP = false;
-        permitRootLogin = "without-password";
-        passwordAuthentication = false;
-        challengeResponseAuthentication = false;
+        firewall = {
+          allowedUDPPorts = [ ];
+          allowedTCPPorts = [ 22 25 80 143 443 8448 ];
+          allowPing = true;
+        };
       };
 
-      xserver.enable = false;
-    };
+      time.timeZone = "Europe/Berlin";
 
-    networking = {
-      extraHosts = ''
-        127.0.0.2 ${config.networking.hostName}
-        127.0.0.3 matrix.${config.networking.hostName}
-      '';
-      firewall = {
-        allowedUDPPorts = [ ];
-        allowedTCPPorts = [ 22 25 80 143 443 8448 ];
-        allowPing = true;
+      users = {
+        defaultUserShell = "/run/current-system/sw/bin/zsh";
       };
     };
-
-    time.timeZone = "Europe/Berlin";
-  
-    users = {
-      defaultUserShell = "/run/current-system/sw/bin/zsh";
-    };
-  };
 }
